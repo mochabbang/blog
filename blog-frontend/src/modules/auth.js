e9dbd1fcbffdbb0ad52a4867fc0573ab4db38de3
mochabbang <sungmin9036@gamil.com>
2022-10-20 09:00:08 +0900

    작업.. 근데 왜 안돼??ㅠㅠ -22.10.20

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,20 +1,46 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
-// 초기 값
-const initialState = {};
-
-// 액션 상태
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+// 액션
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
 // 액션함수
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({form, key, value}) => ({
+    form,     // login, register
+    key,      // username, password, newpassword
+    value,    // 실제 바꾸려는 값
+  })
+)
 
-// reducer
-const auth = handleActions(
-  {
-    [SAMPLE_ACTION]: (state, action) => state,
+export const initializeForm = createAction(INITIALIZE_FORM, form => form);  // register, login
+
+// 초기 값
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: ''
   },
+  login: {
+    username: '',
+    password: ''
+  }
+};
+
+// reduce
+const auth = handleActions({
+  [CHANGE_FIELD]: (state, {payload: {form, key, value}}) => produce(state, draft => {
+    draft[form][key] = [value];     // 예: state.register.username을 바꾼다.
+  }),
+  [INITIALIZE_FORM]: (state, { payload: form }) => ({
+    ...state,
+    [form]: initialState[form],
+  }),
   initialState,
-);
+})
+
 
 export default auth;
